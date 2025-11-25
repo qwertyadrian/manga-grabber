1dc98fe3c6741431ff2f2ec89412f197d0e9a9c9
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2025-11-25 23:02:19 +0400

    Skip UsagiOne tests when running on CI

--- tests/usagi_one_test.py
+++ tests/usagi_one_test.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 import pytest
 import pytest_asyncio
@@ -47,6 +48,7 @@ async def test_get_chapters(chapters):
         assert "branches" in chapter
 
 
+@pytest.mark.skipif(os.getenv("CI") == "true", reason="Skipping download test on CI")
 async def test_download_chapter(tmp_path, chapters):
     chapters, manga = chapters
     chapter = chapters[0]
